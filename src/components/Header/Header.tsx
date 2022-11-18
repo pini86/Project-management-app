5b14dd50a26ac70d5f43d829450d6f843ccff805
User0k <glucus2@yandex.ru>
2022-11-18 06:06:15 +0300

    fix: reset paddings in header Apptoolbar, reset body margins

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,6 +15,7 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import HeaderLink from 'components/buttons/HeaderLink';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const headerBgColor = indigo[900];
 
@@ -27,49 +28,52 @@ export default function Header() {
   };
 
   return (
-    <AppBar sx={{ backgroundColor: headerBgColor, marginBottom: 2, position: 'sticky' }}>
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Stack direction="row" justifyContent="space-between" sx={{ width: '100%' }}>
-            <HeaderLink path="/" text="На главную">
-              <HomeIcon />
-            </HeaderLink>
-            <Stack direction="row">
-              {isAuth ? (
-                <>
-                  <HeaderLink path="/main" text="Доски">
-                    <DashboardIcon />
-                  </HeaderLink>
-                  <HeaderLink path="/profile" text="Профиль">
-                    <AccountCircle />
-                  </HeaderLink>
-                  <HeaderLink path="/" text="Выйти">
-                    <LogoutIcon />
-                  </HeaderLink>
-                </>
-              ) : (
-                <>
-                  <HeaderLink path="/registration" text="Регистрация">
-                    <AccountCircle />
-                  </HeaderLink>
-                  <HeaderLink path="/login" text="Войти">
-                    <LoginIcon />
-                  </HeaderLink>
-                </>
-              )}
-              <Paper elevation={0}>
-                <FormControl>
-                  <InputLabel id="label-language"></InputLabel>
-                  <Select labelId="label-language" value={language} onChange={handleChange}>
-                    <MenuItem value={'RU'}>RU</MenuItem>
-                    <MenuItem value={'EN'}>EN</MenuItem>
-                  </Select>
-                </FormControl>
-              </Paper>
+    <>
+      <CssBaseline />
+      <AppBar sx={{ backgroundColor: headerBgColor, marginBottom: 2, p: 0, position: 'sticky' }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters={true}>
+            <Stack direction="row" justifyContent="space-between" sx={{ width: '100%' }}>
+              <HeaderLink path="/" text="На главную">
+                <HomeIcon />
+              </HeaderLink>
+              <Stack direction="row">
+                {isAuth ? (
+                  <>
+                    <HeaderLink path="/main" text="Доски">
+                      <DashboardIcon />
+                    </HeaderLink>
+                    <HeaderLink path="/profile" text="Профиль">
+                      <AccountCircle />
+                    </HeaderLink>
+                    <HeaderLink path="/" text="Выйти">
+                      <LogoutIcon />
+                    </HeaderLink>
+                  </>
+                ) : (
+                  <>
+                    <HeaderLink path="/registration" text="Регистрация">
+                      <AccountCircle />
+                    </HeaderLink>
+                    <HeaderLink path="/login" text="Войти">
+                      <LoginIcon />
+                    </HeaderLink>
+                  </>
+                )}
+                <Paper elevation={0}>
+                  <FormControl>
+                    <InputLabel id="label-language"></InputLabel>
+                    <Select labelId="label-language" value={language} onChange={handleChange}>
+                      <MenuItem value={'RU'}>RU</MenuItem>
+                      <MenuItem value={'EN'}>EN</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Paper>
+              </Stack>
             </Stack>
-          </Stack>
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 }

fea3075acdced46f963bf2d01b8c2d2f011127c2
User0k <glucus2@yandex.ru>
2022-11-30 03:07:35 +0300

    feat: update board task

--- src/components/BoardTask/BoardTask.tsx
+++ src/components/BoardTask/BoardTask.tsx
@@ -1,19 +1,36 @@
 import { useState } from 'react';
-import { ITaskRefetch } from 'models/Task';
-import { useDeleletTaskByIdMutation } from 'api/TasksApi';
+import { ITaskRefetch, IUpdateTask } from 'models/Task';
+import { useDeleletTaskByIdMutation, useUpdateTaskByIdMutation } from 'api/TasksApi';
+import { useForm } from 'react-hook-form';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import './style.scss';
 
-function BoardTask({ boardId, columnId, _id, title, refetch }: ITaskRefetch) {
+function BoardTask({ boardId, columnId, _id, title, userId, users, refetch }: ITaskRefetch) {
+  type FormValues = {
+    title: string;
+    description: string;
+  };
+
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const [updateTask] = useUpdateTaskByIdMutation();
   const [deleteTask] = useDeleletTaskByIdMutation();
 
   const handleClickOpen = () => {
@@ -30,13 +47,28 @@ function BoardTask({ boardId, columnId, _id, title, refetch }: ITaskRefetch) {
     setIsDeleteModalOpen(false);
   };
 
+  const onUpdateTask = (data: FormValues) => {
+    const newTask: IUpdateTask = {
+      title: data.title,
+      order: 1,
+      description: data.description,
+      userId,
+      users,
+      columnId,
+    };
+    updateTask({ boardId, columnId, taskId: _id, data: newTask });
+    refetch();
+    reset();
+    setIsEditModalOpen(false);
+  };
+
   return (
     <Box>
       <Card className="task-list__item">
         <Box>{title}</Box>
         <Stack>
           <DeleteForeverIcon className="task-delete" onClick={handleClickOpen} />
-          <MoreVertIcon className="task-modify" />
+          <MoreVertIcon className="task-modify" onClick={() => setIsEditModalOpen(true)} />
         </Stack>
       </Card>
       <Dialog
@@ -56,6 +88,43 @@ function BoardTask({ boardId, columnId, _id, title, refetch }: ITaskRefetch) {
             Нет
           </Button>
         </DialogActions>
+      </Dialog>
+      <Dialog open={isEditModalOpen} onClose={() => setIsEditModalOpen(false)}>
+        <DialogTitle id="update-task">{'Редактировать задачу '}</DialogTitle>
+        <form onSubmit={handleSubmit(onUpdateTask)}>
+          <DialogContent>
+            <TextField
+              margin="dense"
+              id="new_title"
+              label="Название задачи"
+              type="text"
+              fullWidth
+              {...register('title', { required: true })}
+            />
+            {errors.title && (
+              <Typography variant="caption" color="error">
+                * Обязательное поле
+              </Typography>
+            )}
+            <TextField
+              margin="dense"
+              id="new_description"
+              label="Описание (необязательно)"
+              type="text"
+              defaultValue=" "
+              fullWidth
+              {...register('description')}
+            />
+          </DialogContent>
+          <DialogActions sx={{ justifyContent: 'space-between' }}>
+            <Button type="submit" variant="contained">
+              Сохранить
+            </Button>
+            <Button onClick={() => setIsEditModalOpen(false)} color="primary" autoFocus>
+              Отмена
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </Box>
   );

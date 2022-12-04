fefbf5dc31525b73198f05c6bc4e4654986fd2b7
Mikhail Druzhinin <pini86@mail.ru>
2022-12-04 16:01:19 +0300

    feat: add DnD first try

---
+++ src/pages/BoardPage/types.ts
@@ -0,0 +1,40 @@
+import type { DraggableId, DraggableLocation } from '@hello-pangea/dnd';
+
+export type Id = string;
+
+export interface AuthorColors {
+  soft: string;
+  hard: string;
+}
+
+export interface Author {
+  id: Id;
+  name: string;
+  avatarUrl: string;
+  url: string;
+  colors: AuthorColors;
+}
+
+//IColumn по нашему, в комментах исходное
+export interface Quote {
+  //id: Id;
+  _id: Id;
+  order: number;
+  //content: string;
+  title: string;
+  //author: Author;
+}
+
+export interface Dragging {
+  id: DraggableId;
+  location: DraggableLocation;
+}
+
+export interface QuoteMap {
+  [key: string]: Quote[];
+}
+
+export interface Task {
+  id: Id;
+  content: string;
+}
